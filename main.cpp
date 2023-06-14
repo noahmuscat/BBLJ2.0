2b5e23b34c15f3aeae5444dd7b50f8827b8ef5cc
Noah Muscat <97421591+noahmuscat@users.noreply.github.com>
2023-06-14 13:58:28 -0400

    Update main.cpp
    additional channels updated

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-/**
- * Name: write_read_loop_with_config.c
- * Desc: Performs an initial call to eWriteNames to write configuration values,
- *       and then calls eWriteNames and eReadNames repeatedly in a loop.
- * Note: For documentation on register names to use, see the T-series Datasheet
- *       or the Modbus Map.
-**/
 
 // For printf
 #include <stdio.h>
+
+// For csv
+#include <fstream>
 
 // For the LabJackM Library
 #include "LabJackM.h"
@@ -19,78 +15,96 @@
 
 int main()
 {
-	int err;
-	int handle;
-	int i;
-	int errorAddress = INITIAL_ERR_ADDRESS;
-	int skippedIntervals;
-	int deviceType;
-	double dacVolt;
-	int fioState;
+    int err;
+    int handle;
+    int i;
+    int errorAddress = INITIAL_ERR_ADDRESS;
+    int skippedIntervals;
+    int deviceType;
 
-	const int INTERVAL_HANDLE = 1;
-	int it = 0;
+    const int INTERVAL_HANDLE = 1;
+    int it = 0;
 
-	// interval time (ms)
-	int msDelay = 100;
+    // interval time (ms)
+    int msDelay = 100;
 
-	enum { MAX_FRAMES = 2 };
-	
-	int numFrames;
-	const char* aNames[MAX_FRAMES];
-	double aValues[MAX_FRAMES];
+    enum { MAX_FRAMES = 9 };
+    
+    int numFrames;
+    const char* aNames[MAX_FRAMES];
+    double aValues[MAX_FRAMES];
 
-	// Open first found LabJack
-	err = LJM_Open(LJM_dtANY, LJM_ctANY, "LJM_idANY", &handle);
-	ErrorCheck(err, "LJM_Open");
+    // Open first found LabJack
+    err = LJM_Open(LJM_dtANY, LJM_ctANY, "LJM_idANY", &handle);
+    ErrorCheck(err, "LJM_Open");
 
-	PrintDeviceInfoFromHandle(handle);
-	printf("\n");
+    PrintDeviceInfoFromHandle(handle);
+    printf("\n");
 
-	deviceType = GetDeviceType(handle);
+    deviceType = GetDeviceType(handle);
+    
+    std::ofstream dataFile("data.csv");
 
-	// Begin the loop
-	printf("Starting loop.  Press Ctrl+c to stop.\n\n");
-	// The LabJackM (LJM) library will catch the Ctrl+c signal, close
-	// all open devices, then exit the program.
+    // Begin the loop
+    printf("Starting loop.  Press Ctrl+c to stop.\n\n");
+    // The LabJackM (LJM) library will catch the Ctrl+c signal, close
+    // all open devices, then exit the program.
 
-	err = LJM_StartInterval(INTERVAL_HANDLE, msDelay * 1000);
-	ErrorCheck(err, "LJM_StartInterval");
+    err = LJM_StartInterval(INTERVAL_HANDLE, msDelay * 1000);
+    ErrorCheck(err, "LJM_StartInterval");
 
-	while (1) {
-		// configures two ports
-		numFrames = 2;
-		aNames[0] = "FIO0";
-		aNames[1] = "AIN0";
-		// reads in values
-		err = LJM_eReadNames(handle, numFrames, (const char **)aNames, aValues,
-			&errorAddress);
-		ErrorCheckWithAddress(err, errorAddress, "LJM_eReadNames");
-		// prints values
-		printf("eReadNames  :");
-		for (i = 0; i < numFrames; i++) {
-			printf(" %s = %.4f  ", aNames[i], aValues[i]);
-		}
-		printf("\n");
+    while (1) {
+        // configures two ports
+        numFrames = 9;
+        aNames[0] = "AIN0";
+        aNames[1] = "FIO0";
+        /*
+        aNames[2] = "";
+        aNames[3] = "";
+        aNames[4] = "";
+        aNames[5] = "";
+        aNames[6] = "";
+        aNames[7] = "";
+        aNames[8] = "";
+        aNames[9] = "";
+        */
+        
+        // reads in values
+        err = LJM_eReadNames(handle, numFrames, (const char **)aNames, aValues,
+            &errorAddress);
+        ErrorCheckWithAddress(err, errorAddress, "LJM_eReadNames");
+        // prints values
+        printf("eReadNames  :");
+        for (i = 0; i < numFrames; i++) {
+            printf(" %s = %.4f  ", aNames[i], aValues[i]);
+        }
+        printf("\n");
+        
+        for (i = 0; i < numFrames; i++) {
+            dataFile << aNames[i] << " = " << aValues[i] << "   ";
+        }
+        dataFile << std::endl;
 
-		++it;
+        ++it;
 
-		err = LJM_WaitForNextInterval(INTERVAL_HANDLE, &skippedIntervals);
-		ErrorCheck(err, "LJM_WaitForNextInterval");
-		if (skippedIntervals > 0) {
-			printf("SkippedIntervals: %d\n", skippedIntervals);
-		}
-		printf("\n");
-	}
+        err = LJM_WaitForNextInterval(INTERVAL_HANDLE, &skippedIntervals);
+        ErrorCheck(err, "LJM_WaitForNextInterval");
+        if (skippedIntervals > 0) {
+            printf("SkippedIntervals: %d\n", skippedIntervals);
+        }
+        printf("\n");
+    }
 
 // Close interval and device handles
-	err = LJM_CleanInterval(INTERVAL_HANDLE);
-	ErrorCheck(err, "LJM_CleanInterval");
+    err = LJM_CleanInterval(INTERVAL_HANDLE);
+    ErrorCheck(err, "LJM_CleanInterval");
 
-	err = LJM_Close(handle);
-	ErrorCheck(err, "LJM_Close");
+    err = LJM_Close(handle);
+    ErrorCheck(err, "LJM_Close");
 
-	WaitForUserIfWindows();
+    WaitForUserIfWindows();
+    
+    dataFile.close();
 
-	return LJME_NOERROR;
+    return LJME_NOERROR;
 }
